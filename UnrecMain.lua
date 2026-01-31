84439c0ddd8bd5915908a61ab124906e37821d68
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-01-31 12:57:35 -0300

    Atualizar o UnrecMain.lua

--- UnrecMain.lua
+++ UnrecMain.lua
@@ -11,22 +11,31 @@ local Window = Rayfield:CreateWindow({
     }
 })
 
--- Carregar modulos
-local Modules = {
-    Clogs = require(script.Modules.Logs)
-    Exploits = require(script.Modules.Exploits),
-    Inject = require(script.Modules.Inject),
-    District = require(script.Modules.District)
-    Muscle = require(script.Modules.Muscle)
-    Tsunami = require(script.Modules.Tsunami)
-    Sharp = require(script.Modules.Sharp)
-
-
+--Tabs
+local Tabs = {
+    Logs     = Window:CreateTab("Logs", 4483362458),
+    Exploits = Window:CreateTab("Exploits", 4483362458),
+    Inject   = Window:CreateTab("Inject", 4483362458),
+    District = Window:CreateTab("District", 4483362458),
+    Muscle   = Window:CreateTab("Muscle", 4483362458),
+    Tsunami  = Window:CreateTab("Tsunami", 4483362458),
+    Sharp    = Window:CreateTab("Sharp", 4483362458)
 }
 
--- Inicializa módulos
-for _, modules in pairs(Modules) do
-    if modules.Init then
-        modules.Init(Window, Tabs)
+--Modulos
+local Modules = {
+    Logs     = require(script.Modules.Logs),
+    Exploits = require(script.Modules.Exploits),
+    Inject   = require(script.Modules.Inject),
+    District = require(script.Modules.District),
+    Muscle   = require(script.Modules.Muscle),
+    Tsunami  = require(script.Modules.Tsunami),
+    Sharp    = require(script.Modules.Sharp)
+}
+
+--inicializar modulos
+for name, module in pairs(Modules) do
+    if module.Init then
+        module.Init(Window, Tabs[name])
     end
 end
