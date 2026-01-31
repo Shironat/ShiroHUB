6ccd3935ec6a21c4fb82fe4bd8122de217a68201
Shiro NAT <manoelrodriguesdev@gmail.com>
2026-01-31 19:58:08 -0300

    Atualizar o Services.lua

--- UnrecModules/Services.lua
+++ UnrecModules/Services.lua
@@ -1,1 +1,11 @@
-sex
+local Services {
+     local Players = game:GetService("Players")
+     local Teams = game:GetService("Teams")
+     local UIS = game:GetService("UserInputService")
+     local RunService = game:GetService("RunService")
+     local VirtualUser = game:GetService("VirtualUser")
+     local ReplicatedStorage = game:GetService("ReplicatedStorage")
+     local player = Players.LocalPlayer
+}
+
+return Services
